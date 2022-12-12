17598dc55e07e2c7c19b074dc6ddb65df49845cb
predavladc <predavladc@gmail.com>
2022-12-12 21:43:18 +0100

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# fullstack-button
+
+A simple fullstack app in under 40 lines of code consisting of a button.
+Deployed at: https://fullstack-button-production.up.railway.app/
---
+++ index.js
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+
+(async () => {
+  mongoose.connect(process.env.MONGO_URL);
+  const buttonSchema = new mongoose.Schema({
+    isOnline: { type: Boolean, default: false },
+  });
+
+  const OnlineSwitch = mongoose.model("isOnline", buttonSchema);
+  let onlineSwitchID = null;
+  let onlineSwitch = await OnlineSwitch.findOne({});
+
+  if (!onlineSwitch) {
+    const newOnlineSwitch = await OnlineSwitch.create({});
+    onlineSwitchID = newOnlineSwitch._id;
+  } else {
+    onlineSwitchID = onlineSwitch._id;
+  }
+
+  const app = express();
+  app.use(cors());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get("/api", async (req, res) => {
+    const onlineSwitch = await OnlineSwitch.findOne({ _id: onlineSwitchID });
+    res.send({ vladIsOnline: onlineSwitch.isOnline });
+  });
+
+  app.post("/switch", async (req, res) => {
+    await OnlineSwitch.updateOne({ _id: onlineSwitchID }, [
+      { $set: { isOnline: { $eq: [false, "$isOnline"] } } },
+    ]);
+    res.status(200).redirect("/");
+  });
+
+  app.listen(process.env.PORT, () =>
+    console.log("button-home-manager running on 9090")
+  );
+})(); // IIFE
